0e2eb159408e546335a5f687cfbf092fe18862b1
vasilisa <vasilisademuanova2700@gmail.com>
2019-11-07 18:05:17 +0300

    change unit tests

--- src/test/java/by/ormedia/entities/RobotTest.java
+++ src/test/java/by/ormedia/entities/RobotTest.java
@@ -1,44 +1,20 @@
 package by.ormedia.entities;
 
-import java.lang.reflect.Field;
 import java.util.Date;
 import junit.framework.TestCase;
 
 public class RobotTest extends TestCase {
 
-	public void testSetName() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	public void testSetGetName() {
 		Robot robot = new Robot();
 		robot.setName("robot");
-		Field field = robot.getClass().getDeclaredField("name");
-		field.setAccessible(true);
-		assertEquals("Fields name didn't match", "robot", field.get(robot));
+		assertEquals("Fields name didn't match", "robot", robot.getName());
 	}
 
-	public void testSetDate() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	public void testSetGetDate() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		Robot robot = new Robot();
 		Date date = new Date();
 		robot.setIssueDate(date);
-		Field field = robot.getClass().getDeclaredField("issueDate");
-		field.setAccessible(true);
-		assertEquals("Fields date created didn't match", date, field.get(robot));
-	}
-
-	public void testGetName()
-			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		Robot robot = new Robot();
-		Field field = robot.getClass().getDeclaredField("name");
-		field.setAccessible(true);
-		field.set(robot, "somename");
-		assertEquals("Fields name didn't match", "somename", robot.getName());
-	}
-
-	public void testGetDate()
-			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		Robot robot = new Robot();
-		Field field = robot.getClass().getDeclaredField("issueDate");
-		field.setAccessible(true);
-		Date date = new Date();
-		field.set(robot, date);
 		assertEquals("Fields date created didn't match", date, robot.getIssueDate());
 	}
 }
